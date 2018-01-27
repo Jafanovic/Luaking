7e01cc9d384c06ea5ca93f0b74515fe55c5a8357
Jaroslav Fait <jardak.fait@gmail.com>
2018-01-27 19:55:07 +0100

    asus big compilation again

--- tex/lua_compile.cmd
+++ tex/lua_compile.cmd
@@ -5,11 +5,11 @@ set root_file=luaking
 timer 
 
 
-lualatex.exe \providecommand{\DebugMode}{true}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file% 
+lualatex.exe \providecommand{\DebugMode}{false}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file% 
 
 biber %root_file%.bcf
 
-lualatex.exe \providecommand{\DebugMode}{true}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file%
+lualatex.exe \providecommand{\DebugMode}{false}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file%
 
 timer /s /nologo
 
--- tex/lua_latexmk.cmd
+++ tex/lua_latexmk.cmd
@@ -1,8 +1,10 @@
 echo WIKING XELATEX compile
+SETLOCAL
+set root_file=luaking
 :: -pvc:   mnemonic: "previewcontinuously"
 :: -e:     Execute the speciﬁed initialization code before processing.  
 :: -pdf:   Generate pdf version of document using pdﬂatex.
 :: -pdﬂua: Generate pdf version of document using lualatex.
 :: -pdfxe: Generate pdf version of document using xelatex.
 :: -time:  ShowCPU time used
-latexmk -pvc -pdflua -e "$pdflatex='lualatex -interaction=nonstopmode -halt-on-error'" luaking.tex 
+latexmk -pvc -pdflua -e "$pdflatex='lualatex -interaction=nonstopmode -halt-on-error \providecommand{\DebugMode}{true}\input{%root_file%.tex}'" %root_file%.tex 
