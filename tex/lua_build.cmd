cd5387632cd57ad606cdef3d27c77d7498ef76f6
Jaroslav Fait <jardak.fait@gmail.com>
2020-01-02 19:01:50 +0100

    repo cleaning

---
+++ tex/lua_build.cmd
@@ -0,0 +1,41 @@
+echo WIKING LuaTEX compile
+SETLOCAL
+set root_file=luaking
+set start_time=%time%
+
+echo COMPILATION START: %start_time%
+echo FIRST ROUND   
+color 07
+lualatex.exe \providecommand{\DebugMode}{false}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file% 
+
+echo Generate requested bibliography
+biber %root_file%.bcf
+
+echo SECOND ROUND   
+color 06
+lualatex.exe \providecommand{\DebugMode}{false}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file%
+
+echo THIRD ROUND   
+color 0A
+lualatex.exe \providecommand{\DebugMode}{false}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file%
+
+echo Copy files to Build folder   
+COPY ".\%root_file%.pdf" "..\build\%root_file%_%date:~-4,4%_%date:~-7,2%_%date:~-10,2%.pdf"
+COPY ".\%root_file%.log" "..\build\%root_file%_%date:~-4,4%_%date:~-7,2%_%date:~-10,2%.log"
+
+gswin64 -o %root_file%_gray.pdf^
+   -sDEVICE=pdfwrite^
+   -sColorConversionStrategy=Gray^
+   -dProcessColorModel=/DeviceGray^
+   -dCompatibilityLevel=1.4^
+    luaking.pdf
+	
+COPY ".\%root_file%_gray.pdf" "..\build\%root_file%_%date:~-4,4%_%date:~-7,2%_%date:~-10,2%_gray.pdf"
+DEL luaking*.pdf
+
+START cleaner.cmd
+
+echo COMPILATION STOP: %time%
+ENDLOCAL
+@echo off
+pause
--- tex/lua_debug.cmd
+++ tex/lua_debug.cmd
@@ -10,11 +10,6 @@ biber %root_file%.bcf
 
 lualatex.exe \providecommand{\DebugMode}{true}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file%
 
-timer /s /nologo
-
-rem COPY ".\%root_file%.pdf" "..\..\%root_file%_%date:~-4,4%_%date:~-7,2%_%date:~-10,2%.pdf"
-rem COPY ".\%root_file%.log" "..\..\%root_file%_%date:~-4,4%_%date:~-7,2%_%date:~-10,2%.log"
-
 
 echo STOP %time%
 ENDLOCAL
