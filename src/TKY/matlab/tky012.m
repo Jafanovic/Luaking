1b5e248466f05426edacc836de02e24436a5c08b
Jaroslav Fait <jardak.fait@gmail.com>
2020-05-20 22:03:43 +0200

    TKY09

--- src/TKY/matlab/tky012.m
+++ src/TKY/matlab/tky012.m
@@ -1,4 +1,4 @@
-    N=[1,0,0];
+N=[1,0,0];
 D=[75.579367, -137.90481, 63.325442];
 T = 2e-6;
 [ip, t] = impz(N,D,100, 1/T);
@@ -6,4 +6,4 @@ stem(t,ip)
 grid
 xlabel 't'
 ylabel 'h'
-title 'Impulsní charakteristika'
+title 'Impulsni charakteristika'
