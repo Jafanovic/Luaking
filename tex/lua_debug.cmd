b6d76d09c2a857252d5ad8cd11780a9b0f4f7027
Jaroslav Fait <jardak.fait@gmail.com>
2020-08-23 22:28:49 +0200

    MAI03

--- tex/lua_debug.cmd
+++ tex/lua_debug.cmd
@@ -6,11 +6,6 @@ set start_time=%time%
 echo START %start_time%
 lualatex.exe \providecommand{\DebugMode}{true}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file% 
 
-biber %root_file%.bcf
-
-lualatex.exe \providecommand{\DebugMode}{true}\input{%root_file%.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=%root_file%
-
-
 echo STOP %time%
 ENDLOCAL
 @echo off
