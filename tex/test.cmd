8e50b01801c0146b29fbf42fc4e1e0e91db08384
Jaroslav Fait <jaroslav.fait@hotmail.com>
2021-05-03 15:32:57 +0000

    Create test.cmd

---
+++ tex/test.cmd
@@ -0,0 +1,13 @@
+echo Multibook compile 
+@echo OFF
+REM Batch file for loop – looping through a range of values
+REM http://www.trytoprogram.com/batch-file-for-loop/#example
+set x=4
+FOR /L %%y IN (1, 1, %x%) DO (
+	ECHO LOOP %%y   
+	color 06
+	lualatex.exe \providecommand{\DebugMode}{false}\input{vol0%%y.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=vol0%%y 
+	echo Generate requested bibliography
+	biber vol0%%y.bcf
+)
+PAUSE
