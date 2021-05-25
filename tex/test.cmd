356fffd60291240008be1c5629eb3dae90952df2
Jaroslav Fait <jaroslav.fait@hotmail.com>
2021-05-25 11:21:22 +0000

    zzt

--- tex/test.cmd
+++ tex/test.cmd
@@ -6,8 +6,12 @@ set x=4
 FOR /L %%y IN (1, 1, %x%) DO (
 	ECHO LOOP %%y   
 	color %%y
-	lualatex.exe \providecommand{\DebugMode}{false}\input{vol0%%y.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=vol0%%y 
+	lualatex.exe \providecommand{\DebugMode}{false}\input{vol0%%y.tex} -synctex=1 -interaction=nonstopmode -halt-on-error -jobname=vol0%%y
 	echo Generate requested bibliography
 	biber vol0%%y.bcf
+	echo Copy files to Build folder   
+	COPY ".\vol0%%y.pdf" "..\build\vol0%%y_%date:~-4,4%_%date:~-7,2%_%date:~-10,2%.pdf"
+	COPY ".\vol0%%y.log" "..\build\vol0%%y_%date:~-4,4%_%date:~-7,2%_%date:~-10,2%.log"
 )
 PAUSE
+ 
